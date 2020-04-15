763d0ecb1e779208fc980bb8992b2351ec349fb0
olgn <tealhobsonlowther@gmail.com>
2020-04-14 22:36:56 -0700

    add initial visualization app

---
+++ visualizations/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Network from './components/features/network/Network';
+
+function App() {
+  return (
+    <div className="App">
+      <Network />
+    </div>
+  );
+}
+
+export default App;
---
+++ visualizations/src/components/features/network/Network.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import withSQLite from '../../wrappers/withSQLite'
+
+const Network = () => {
+    return (
+        <div className="networkWrapper">
+            Wow!
+        </div>
+    )
+}
+
+export default withSQLite(Network)
